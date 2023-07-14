1136471cd40e8a59bb65c00270bc393b2f18256d
homvi <honvedoadamkj@gmail.com>
2023-07-14 08:43:49 +0200

    add profile page and add conditionals to protecteed route

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,5 +1,19 @@
+import { UserAuth } from "../context/AuthContext";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = UserAuth();
+  
+  return (
+    <>
+      {user && (
+        <div className="container flex flex-col gap-3 p-5">
+          <img className="w-24" src={user.photoURL} alt="photo" />
+          <div>Name: {user.displayName}</div>
+          <div>Email: {user.email}</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Profile;
--- src/Pages/Protected.jsx
+++ src/Pages/Protected.jsx
@@ -1,23 +1,38 @@
 import { UserAuth } from "../context/AuthContext";
 
 const Protected = () => {
-  const { user } = UserAuth();
+  const { user, googleSignIn } = UserAuth();
+
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://i.pravatar.cc/500"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
+        {user && (
+          <img
+            src="https://i.pravatar.cc/300"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+        )}
+
         <div>
           <h1 className="text-5xl font-bold">This is a protected route</h1>
           {user ? (
-            <p className="py-6">You are alloved to see it</p>
+            <p className="py-6">You are alloved to see it {user.displayName}</p>
           ) : (
             <p className="py-6">Sign it to see</p>
           )}
-          <button className="btn btn-primary">Get Started</button>
+          {!user && (
+            <button onClick={handleSignIn} className="btn btn-primary">
+              Sign in
+            </button>
+          )}
         </div>
       </div>
     </div>
